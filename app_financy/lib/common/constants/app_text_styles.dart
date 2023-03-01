6ab9ad357080349ad7c7ed7bdf94e83f1f59b367
Tiago Pereira Barreto <93627992+TiagoPBarreto@users.noreply.github.com>
2023-03-01 09:36:21 -0300

    Page SplashScreen

---
+++ app_financy/lib/common/constants/app_text_styles.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/cupertino.dart';
+
+import 'dart:ui';
+
+class AppTextStyles {
+  AppTextStyles._();
+  static const TextStyle bigText =
+      TextStyle(fontFamily: 'Inter',fontSize: 50.0, fontWeight: FontWeight.w700);
+}
